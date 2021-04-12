281651bcc5831e3a60fd0164256da1af7ef7ee6d
cjmazzarri <cjmazzarri@gmail.com>
2021-04-12 17:53:49 -0500

    Added Selection, Fitness, and Breed functions to main .py file

--- IVMax.py
+++ IVMax.py
@@ -12,3 +12,51 @@ def Pair(N):
         pair[i]=randomN[i]
         pair[randomN[i]]=i
     return pair
+
+def Inicialization(N):
+    global Subjects
+    global Best
+    Subjects = {}
+    for i in range(N):
+        Subjects[i] = np.random.choice(32,6,replace=True)
+    print('---Inicializaction----')
+    Show(N)
+
+#Compara la idoneidad de 2 individuos, escogiendo al de mayor idoneidad
+def Selection(N):
+    Pair = Pairs(N)
+    print('Pair',Pair)
+    for k,v in Pair.items():
+        if Fitness(Subjects[k]) >= Fitness(Subjects[v]):
+            Subjects[v] = Subjects[k]
+    Show(N)
+
+#Calcula idoneidad según la suma de los IVs de un individuo
+def Fitness(iv_group):
+    average_ivs=0
+    for i, _ in enumerate(iv_group):
+        average_ivs+=iv_group[i]
+    return average_ivs
+
+#Cruza a 2 individuos, asignando el punto de corte de los IVs y generando
+#2 hijos
+def Breed(N):
+    Pair = Pairs(N)
+    print('Pair:', Pair)
+    item = 0
+    for k,v in Pair.items():
+        if item % 2 == 0:
+            cut_point = random.randint(1,4) #pto de corte de 1-4 porque se tienen 6 IVs
+            print('cut point',cut_point)
+            Child1 = []
+            Child2 = []
+            Parent1 = Subjects[k]
+            Parent2 = Subjects[v]
+            Child1.extend(Child1[0:cut_point])
+            Child1.extend(Child2[cut_point:])
+            Child2.extend(Child2[0:cut_point])
+            Child2.extend(Child1[cut_point:])
+            Subjects[k] = Child1
+            Subjects[v] = Child2
+        item = item+1
+    Show(N)

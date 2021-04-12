bdd5fbb71bbf11557b3ba54fa08ec5c8311bea8d
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-04-12 18:36:00 -0500

    Build IVMax.

--- IVMax.py
+++ IVMax.py
@@ -1,7 +1,18 @@
 import numpy as np
 import random
+from copy import deepcopy as dc
 
-def Pair(N):
+PokemonList=[]
+PokemonList.append(Pokemon("Torchic", [45, 60, 40, 70, 50, 45]))
+PokemonList.append(Pokemon("Squirtle", [44, 48, 65, 50, 64, 43]))
+PokemonList.append(Pokemon("Turtwig", [55, 68, 64, 45, 55, 31]))
+
+class Pokemon():
+    def __init__(self, name, stats):
+        self.name=name
+        self.stats=dc(stats)
+
+def Pairs(N):
     # Retorna una lista en orden aleatoria de la segunda mitad de los individuos
     # ej: N=8 -> [7, 6, 5, 4]
     randomN=random.sample(range(int(N/2), N), int(N/2))
@@ -19,7 +30,7 @@ def Inicialization(N):
     Subjects = {}
     for i in range(N):
         Subjects[i] = np.random.choice(32,6,replace=True)
-    print('---Inicializaction----')
+    print('---Inicialization----')
     Show(N)
 
 #Compara la idoneidad de 2 individuos, escogiendo al de mayor idoneidad
@@ -52,25 +63,49 @@ def Breed(N):
             Child2 = []
             Parent1 = Subjects[k]
             Parent2 = Subjects[v]
-            Child1.extend(Child1[0:cut_point])
-            Child1.extend(Child2[cut_point:])
-            Child2.extend(Child2[0:cut_point])
-            Child2.extend(Child1[cut_point:])
+            Child1.extend(Parent1[0:cut_point])
+            Child1.extend(Parent2[cut_point:])
+            Child2.extend(Parent2[0:cut_point])
+            Child2.extend(Parent1[cut_point:])
             Subjects[k] = Child1
             Subjects[v] = Child2
         item = item+1
     Show(N)
 
+#Muta la mitad de individuos (50%-variable) en una posicion aleatoria con un valor entre 0-31
 def Mutation(N):
     print('-----Mutation ----')
     for i in range(int(N/2)):
         ChooseSubj = random.randint(0,N-1)
-        #print(ChooseSubj)
         ChoosePos = random.randint(0,5)
-        #print(ChoosePos)
         ChooseVal = random.randint(0, 31)
-        #print(ChooseVal)
         Subjects[ChooseSubj][ChoosePos] = ChooseVal
         print('The subject {}, in the position {}, with a value of {}, was mutated'.format(ChooseSubj, ChoosePos, ChooseVal))
-        #print("****")
     Show(N)
+
+def Show(N):
+    for i in range(N):
+        print(Subjects[i],'f(x)=',Fitness(Subjects[i]))
+
+def ShowBest(N):
+    index=-1
+    maxFit=0
+    for i in range(N):
+        if maxFit<Fitness(Subjects[i]):
+            maxFit=Fitness(Subjects[i])
+            index=i
+    print("Best Subject:",Subjects[index])
+    return Subjects[index]
+
+#def ShowStats(iv_group):
+
+
+N=6
+generations=5
+Inicialization(N)
+for i in range(generations):
+    print('\nGeneration {}:'.format(i+1))
+    Selection(N)
+    Breed(N)
+    Mutation(N)
+    ShowBest(N)
